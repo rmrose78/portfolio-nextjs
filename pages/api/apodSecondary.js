f154b07466a0d31f8d4261629d332a54c712cf65
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-06-28 14:00:33 -0400

    Changes for Netlify env variables

--- pages/api/apodSecondary.js
+++ pages/api/apodSecondary.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const data = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${process.env.NEXT_PUBLIC_APOD_API_KEY_SECONDARY}`
+    `https://api.nasa.gov/planetary/apod?api_key=${process.env.NODE_ENV_APOD_API_KEY_SECONDARY}`
   ).then((response) => response.json());
   res.status(200).json(data);
 }
