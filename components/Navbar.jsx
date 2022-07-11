5cca6eb5250a6dbfb9e838fb5a642dd635fbe42c
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-07-11 19:58:33 -0400

    Content size constraint added, need more testing for ultrawide

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,43 +20,40 @@ const Navbar = () => {
     <>
       <Menu handleMenuClick={handleMenuClick} menuClick={menuClick} />
       <div className="nav border-b border-lm-tertiary dark:border-dm-tertiary bg-lm-secondary dark:bg-dm-secondary">
-        <div className="nameLogoCtr">
-          <Link href={"/"}>
-            <span className="nav__name text-lm-text-secondary dark:text-dm-text-secondary hover:text-lm-tertiary dark:hover:text-dm-tertiary">
-              <span className="nav__nameR text-lm-tertiary dark:text-dm-tertiary">
-                R
+        <div className="nav__contents">
+          <div className="nameLogoCtr">
+            <Link href={"/"}>
+              <span className="nav__name text-lm-text-secondary dark:text-dm-text-secondary hover:text-lm-tertiary dark:hover:text-dm-tertiary">
+                <span className="nav__nameR text-lm-tertiary dark:text-dm-tertiary">
+                  R
+                </span>
+                YAN
+                <span className="nav__nameR text-lm-tertiary dark:text-dm-tertiary">
+                  R
+                </span>
+                OSE
               </span>
-              YAN
-              <span className="nav__nameR text-lm-tertiary dark:text-dm-tertiary">
-                R
-              </span>
-              OSE
-            </span>
-          </Link>
-        </div>
+            </Link>
+          </div>
+          {/* Page List */}
+          <div>
+            <ul className="list text-lm-text-secondary dark:text-dm-text-secondary">
+              <NavElement href="/" pathname={pathname}>
+                Home
+              </NavElement>
+              <NavElement href="/about" pathname={pathname}>
+                About
+              </NavElement>
+              <NavElement href="/contact" pathname={pathname}>
+                Contact
+              </NavElement>
+              <DarkmodeToggle />
+            </ul>
 
-        {/* Page List */}
-        <div>
-          <ul className="list text-lm-text-secondary dark:text-dm-text-secondary">
-            <NavElement href="/" pathname={pathname}>
-              Home
-            </NavElement>
-            <NavElement href="/about" pathname={pathname}>
-              About
-            </NavElement>
-            <NavElement href="/contact" pathname={pathname}>
-              Contact
-            </NavElement>
-            <DarkmodeToggle />
-          </ul>
-
-          {/* Hamburger */}
-          <div className="hamburgerCtr" onClick={handleMenuClick}>
-            <Hamburger
-              color={
-                theme === "dark" ? "hsl(39, 100%, 75%)" : "hsl(39, 20%, 20%)"
-              }
-            />
+            {/* Hamburger */}
+            <div className="hamburgerCtr" onClick={handleMenuClick}>
+              <Hamburger color={theme === "dark" ? "#ffd280" : "#3d3629"} />
+            </div>
           </div>
         </div>
       </div>
