6730258459c4f7692c11c226019ab8dff59b4d67
Ryan Rose <rmrose78@gmail.com>
2023-04-15 21:32:11 -0400

    commiting changes

--- next.config.js
+++ next.config.js
@@ -4,9 +4,17 @@ const nextConfig = {
   images: {
     domains: ["apod.nasa.gov"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Add support for TypeScript files
+      config.resolve.extensions.push(".ts", ".tsx");
+    }
+    return config;
+  },
 };
 
 // module.exports = nextConfig;
 module.exports = {
   distDir: "build",
+  ...nextConfig,
 };
